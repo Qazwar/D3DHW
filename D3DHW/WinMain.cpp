54545158314eed03eaa3929b159b77790e9daead
traviszeng <traviszeng7@gmail.com>
2020-01-27 11:18:19 +0800

    tutorial 2 show a window

--- D3DHW/WinMain.cpp
+++ D3DHW/WinMain.cpp
@@ -7,5 +7,32 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
+	const auto pClassName = "D3DHW";
+	//register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hCursor = nullptr;
+	wc.hIcon = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	RegisterClassEx(&wc);
+	
+	//create window instance 
+	HWND hWnd = CreateWindowEx(
+		0,pClassName,
+		"Happy Hard Window",
+		WS_CAPTION|WS_MINIMIZEBOX|WS_SYSMENU,
+		200,200,640,480,
+		nullptr,nullptr,hInstance,nullptr
+	);
+	//show window
+	ShowWindow(hWnd, SW_SHOW);
+	while (true);
 	return 0;
 }
