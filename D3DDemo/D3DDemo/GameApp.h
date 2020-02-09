3dc2059cafaf07f40b4ecf3c4f0300b1e79b5aaa
traviszeng <traviszeng7@gmail.com>
2020-02-09 19:59:51 +0800

    03 Rendering a cube

--- D3DDemo/D3DDemo/GameApp.h
+++ D3DDemo/D3DDemo/GameApp.h
@@ -13,6 +13,13 @@ public:
 		static const D3D11_INPUT_ELEMENT_DESC inputLayout[2];
 	};
 
+	struct ConstantBuffer
+	{
+		DirectX::XMMATRIX world;
+		DirectX::XMMATRIX view;
+		DirectX::XMMATRIX proj;
+	};
+
 public:
 	GameApp(HINSTANCE hInstance);
 	~GameApp();
@@ -30,10 +37,14 @@ private:
 
 
 private:
-	ComPtr<ID3D11InputLayout> m_pVertexLayout;	// 顶点输入布局
-	ComPtr<ID3D11Buffer> m_pVertexBuffer;		// 顶点缓冲区
-	ComPtr<ID3D11VertexShader> m_pVertexShader;	// 顶点着色器
-	ComPtr<ID3D11PixelShader> m_pPixelShader;	// 像素着色器
+	ComPtr<ID3D11InputLayout> m_pVertexLayout;	    // 顶点输入布局
+	ComPtr<ID3D11Buffer> m_pVertexBuffer;			// 顶点缓冲区
+	ComPtr<ID3D11Buffer> m_pIndexBuffer;			// 索引缓冲区
+	ComPtr<ID3D11Buffer> m_pConstantBuffer;		    // 常量缓冲区
+
+	ComPtr<ID3D11VertexShader> m_pVertexShader;	    // 顶点着色器
+	ComPtr<ID3D11PixelShader> m_pPixelShader;		// 像素着色器
+	ConstantBuffer m_CBuffer;	                    // 用于修改GPU常量缓冲区的变量
 };
 
 
